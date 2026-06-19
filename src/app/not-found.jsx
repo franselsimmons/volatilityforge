e5082a31ef7d007726ae7e58347c2d235de35ea4
franselsimmons <lrsdaktechniek@hotmail.com>
2026-06-19 20:15:23 +0200

    Update not-found.jsx

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,16 +4,14 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <main className="adminShell">
-      <section className="loginCard">
-        <div className="brand center">
+    <main className="centerPage">
+      <section className="smallPanel">
+        <div className="brand brandCenter">
           <span className="brandMark">VF</span>
           <span>VolatilityForge</span>
         </div>
-
         <h1>404</h1>
         <p>Deze pagina bestaat niet.</p>
-
         <Link className="primaryButton" href="/en">
           Terug naar homepage
         </Link>
